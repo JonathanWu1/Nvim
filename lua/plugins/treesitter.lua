c722231d8d6ff77513c3d19d960ca6bd9c80b841
JonathanWu1 <wujo2012@gmail.com>
2025-12-14 18:36:28 +0100

    update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,6 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs',
     opts = {
         ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'c_sharp', 'tsx', 'typescript', 'scss' },
         auto_install = true,
